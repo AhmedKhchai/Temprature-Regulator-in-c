14c5bb36f9458ef1b6d7b2fdc16b9e92937a1935
AhmedKhchai <a.khchai@gmail.com>
2022-11-23 15:48:30 +0100

    loop

--- main.c
+++ main.c
@@ -5,41 +5,23 @@
 #include <time.h>
 # define TEMP_OK 0
 # define CHAUFFER 64
-# define REFROIDIR 128
+# define REFROIDIR 1 << 7
 
+struct sigaction sig;
+int temp_target;
 int tempGenarator(int lower, int upper)
 {
-    int num;
-    return  num = (rand() % (upper - lower + 1)) + lower;
+    return (rand() % (upper - lower + 1)) + lower;
 }
 void toBinary(unsigned char result){
   int binary[8];
   for(int n = 0; n < 8; n++)
     binary[7-n] = (result >> n) & 1;
-  /* print result */
   for(int n = 0; n < 8; n++)
     printf("%d", binary[n]);
   printf("\n");
 }
-
-int ecouteAlarm(int, int);
-
-int main(int argc , char **argv){
-    int temp_target;
-    printf("Quelle est votre temperature preferee ?: \n");
-    scanf("%d", &temp_target);
-    while(1){
-        struct sigaction sig;
-        sig.sa_handler = ecouteAlarm(&sig, temp_target);
-        sig.sa_flags = 5;
-        sigaction(SIGALRM,&sig,NULL);
-        printf("Signal \n");
-        alarm(1);
-	}
-	return 0;
-}
-
-int ecouteAlarm(sig, temp_target){
+int ecouteAlarm(sig){
     int lower = 15, upper = 25;
     unsigned char result = TEMP_OK;
     srand(time(0));
@@ -52,11 +34,10 @@ int ecouteAlarm(sig, temp_target){
             result |= 1 << 1;
         }else if( abs(diff) == 2){
             result |= 1 << 0;
-        }else{
+        }else if (abs(diff) > 2){
             result |= 1 << 0;
             result |= 1 << 1;
         }
-        // printf("RESULT: 0 \n");
         printf("RESULT: %u ,DIFF: %d\n", result, abs(diff));
         toBinary(result);
     }else if (temp_current > temp_target){
@@ -66,11 +47,10 @@ int ecouteAlarm(sig, temp_target){
             result |= 1 << 1;
         }else if( abs(diff) == 2){
             result |= 1 << 0;
-        }else{
+        }else if (abs(diff) > 2){
             result |= 1 << 0;
             result |= 1 << 1;
         }
-        // printf("RESULT: 128 \n");
         printf("RESULT: %u ,DIFF: %d\n", result, abs(diff));
         toBinary(result);
     }else{
@@ -80,17 +60,30 @@ int ecouteAlarm(sig, temp_target){
             result |= 1 << 1;
         }else if( abs(diff) == 2){
             result |= 1 << 0;
-        }else{
+        }else if (abs(diff) > 2){
             result |= 1 << 0;
             result |= 1 << 1;
         }
-        // printf("RESULT: 64 \n");
         printf("RESULT: %u ,DIFF: %d\n", result, abs(diff));
         toBinary(result);
     }
 	printf("ALARM \n");
     printf("Temp Target %d: \n", temp_target);
     printf("Temp: %d \n", temp_current);
-	sigaction(SIGALRM,&sig,NULL);
+	alarm(2);
 
 }
+
+int main(int argc , char **argv){
+    printf("Quelle est votre temperature preferee ?: \n");
+    scanf("%d", &temp_target);
+        sig.sa_handler = &ecouteAlarm;
+        sig.sa_flags = 0;
+        sigaction(SIGALRM,&sig,NULL);
+        alarm(2);
+    while(1){
+	}
+	return 0;
+}
+
+
